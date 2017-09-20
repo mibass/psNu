763191a585411474e116c9799b2cab6ddae03c5d
Matthew Bass <mbass@apollo.phy.bnl.gov>
2017-09-20 17:32:39 -0400

    Adding new analytical smearing and switched Binned over to it. Removed upper (variable) bins in Binned.

--- smearing_tools.py
+++ smearing_tools.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import numpy as np
-
+from math import *
 
 def smearingA(Ebins, sigma ):
     dim=len(Ebins)-1
@@ -14,7 +14,7 @@ def smearingA(Ebins, sigma ):
         #store in matrix, normalizing each column to 1
         m[:,i]=h/sum(h)
 
-    return m                        
+    return m
 
 def smearingB(Ebins, sigma, missing ):
     dim=len(Ebins)-1
@@ -28,4 +28,19 @@ def smearingB(Ebins, sigma, missing ):
         #store in matrix, normalizing each row to 1
         m[:,i]=h/sum(h)
 
+    return m
+
+def asmearing(Ebins, sigma, bias):
+    dim=len(Ebins)-1
+    m=np.zeros((dim,dim))
+    for i in range(dim):
+        e1=(Ebins[i]+Ebins[i+1])/2
+        h=np.zeros(dim)
+        for j in range(dim):
+            e2=(Ebins[j]+Ebins[j+1])/2
+            h[j]=exp(-pow(e2-e1,2)/(2*sigma*sigma*e1*e1))
+            if(e2<=e1):
+                #h[j]+=e1*bias
+                h[j]+=bias*exp(-pow(e2-e1,2)/(2*e1*e1))
+        m[:,i]=h/sum(h)
     return m
